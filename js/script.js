fcb83ef79266c72df3233586c4251322d3d0386c
Laz <nyys811@aol.com>
2023-06-14 22:49:17 -0400

    added a function for the date display

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-    
+
     var timeBlocks = $('.time-block');
     var saveBtns = $('.saveBtn');
     var currentDayEl = $('#currentDay');
+
+    function displayCurrentDay() {
+        var currentDay = dayjs().format('dddd, MMMM D, YYYY');
+        currentDayEl.text(currentDay);
+      }
+
+      
 
 });
