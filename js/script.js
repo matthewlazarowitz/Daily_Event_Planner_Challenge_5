0d0ae1cd6281a73653fd4ca5aa4f4d613dbe18b3
Laz <nyys811@aol.com>
2023-06-14 22:58:16 -0400

    added function to save the events to local storage & general formatting

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     var timeBlocks = $('.time-block');
     var saveBtns = $('.saveBtn');
@@ -7,16 +7,21 @@ $(document).ready(function() {
     function displayCurrentDay() {
         var currentDay = dayjs().format('dddd, MMMM D, YYYY');
         currentDayEl.text(currentDay);
-      }
+    }
 
-      function getEvents() {
-       
+    function getEvents() {
+
         var events = localStorage.getItem('events');
-        
         var parsed = JSON.parse(events);
-    
-       
+
         return parsed || {};
-      }
+    }
+
+    function saveEvent(hour, eventDescription) {
+
+        var events = getEvents();
+        events[hour] = eventDescription;
+        localStorage.setItem('events', JSON.stringify(events));
+    }
 
 });
